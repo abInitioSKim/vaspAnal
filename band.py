88dce6bbd3901d566f35c1a836b7cff081297b07
abInitioSKim <kimsunghyun@kaist.ac.kr>
2014-03-28 20:53:08 +0900

    add argparse

--- band.py
+++ band.py
@@ -2,25 +2,27 @@
 from vaspData import *
 import matplotlib.pyplot as plt
 import sys
+import argparse
 
-if len(sys.argv)>2:
-    EIGENVAL = sys.argv[1]
-    
-else: 
-    EIGENVAL = "EIGENVAL"
+if __name__=='__main__':
 
-kpts, eigvals = readEIGENVAL(EIGENVAL)
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-f","--file", help="eigenval file path is needed. default is ./eigenval",default = "./EIGENVAL")
+    args = parser.parse_args()
+    eigenval_path = args.file
 
-kpts.append(kpts[0])
-x = [ np.linalg.norm(kpts[i] - kpts[i-1]) for i,k in enumerate(kpts)]
-x = x[:-1]
-x = [dist if dist<0.3 else 0 for dist in x]
-x = np.cumsum(x)
+    kpts, eigvals = readEIGENVAL(eigenval_path)
 
-X = np.tile(x,eigvals.shape[1])
-Y = eigvals.T.flatten()
-fig = plt.figure()
+    kpts.append(kpts[0])
+    x = [ np.linalg.norm(kpts[i] - kpts[i-1]) for i,k in enumerate(kpts)]
+    x = x[:-1]
+    x = [dist if dist<0.3 else 0 for dist in x]
+    x = np.cumsum(x)
 
-plt.plot(X,Y,'ro', alpha = 0.3)
-#fig.savefig(sys.argv[2])
-plt.show()
+    X = np.tile(x,eigvals.shape[1])
+    Y = eigvals.T.flatten()
+    fig = plt.figure()
+
+    plt.plot(X,Y,'ro', alpha = 0.3)
+    #fig.savefig(sys.argv[2])
+    plt.show()
