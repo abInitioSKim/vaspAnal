3407659cd84a80946bee3fa3df12d6924136027e
abinitioSKim <kimsunghyun@kaist.ac.kr>
2014-11-28 16:32:47 +0900

    log scale supported vmax vmin argument supported

--- ldos.py
+++ ldos.py
@@ -8,7 +8,7 @@ def gauss(x, a, mean, sigma):
     from scipy.stats import norm
     return a * norm.pdf(x, mean, sigma)
 
-def get_ldos(path, extent, sigma=0.1, e_grid=100):
+def get_ldos(path, extent, sigma=0.1, e_grid=100, axis=0):
     '''
     read PARCHG file in the path
     return ldos along x
@@ -35,6 +35,9 @@ def get_ldos(path, extent, sigma=0.1, e_grid=100):
 
     print num_eig, 'PARCHG will be read'
     num = 0
+    axis_sum = [0, 1, 2]
+    axis_sum.pop(axis)
+    print axis_sum
     for kpt_index, eigenval_kpt in enumerate(eigenval):
         # if kpt_index > 0:
         #     continue
@@ -50,7 +53,7 @@ def get_ldos(path, extent, sigma=0.1, e_grid=100):
                         readCHGCAR(path + parchg_file)
 
 
-                    chg_sum = np.sum(CHGCAR, axis=(1,2))
+                    chg_sum = np.sum(CHGCAR, axis=tuple(axis_sum))
                     x_grid = len(chg_sum)
                     x_space = (x_max - x_min) / float(x_grid)
                     chg_sum *= weight[kpt_index] / 2. / np.prod(CHGCAR.shape) \
@@ -60,6 +63,8 @@ def get_ldos(path, extent, sigma=0.1, e_grid=100):
                 else:
                     print parchg_file, "does not exists. check energy range",\
                           "or your parchg calculations"
+
+    x_max = np.linalg.norm(lattice_matrix[axis]) * lat_const
 
     # smoothing charge densities along energy axis
     ldos_interpol = np.zeros((x_grid, e_grid))
@@ -74,13 +79,15 @@ def get_ldos(path, extent, sigma=0.1, e_grid=100):
         ldos_interpol[x_index, :] = temp_x[::-1]
     return ldos_interpol
 
-def draw_ldos(ldos, extent, vmin=0., vmax=1., ticks=None):
+def draw_ldos(ldos, extent_calc, extent_show, 
+              vmin=0., vmax=1., ticks=None, log_scale=False):
     from matplotlib import rcParams, gridspec
+    from matplotlib.colors import LogNorm
     import mpl_toolkits.axisartist as axisartist
     aspect = 20
 
     vmax = ldos.max() * vmax
-    vmin = ldos.min() * vmin
+    vmin = ldos.max() * vmin
     # eMin , eMax = extent[2:]
 
     fig = plt.figure(figsize=(7, 5))
@@ -92,13 +99,17 @@ def draw_ldos(ldos, extent, vmin=0., vmax=1., ticks=None):
     ax_cbar = plt.subplot2grid((1,aspect), (0,aspect-1))#(111)
     ax_ldos.set_ylabel('Energy (eV)')
     ax_ldos.set_xlabel('x ($\AA$)') 
+    print vmin, vmax
+    if log_scale:
+        im = ax_ldos.imshow(ldos.T, aspect='auto', 
+               extent=extent_calc, norm=LogNorm(vmin=vmin, vmax=vmax))
+    else:
+        im = ax_ldos.imshow(ldos.T, aspect='auto', 
+               extent=extent_calc, vmin=vmin+0.01, vmax=vmax)
 
-    im = ax_ldos.imshow(ldos.T, aspect='auto', 
-           extent=extent, vmin=vmin, vmax=vmax)
+    ax_ldos.axis(extent_show)
 
     # draw color bar
-    # ax_cbar= fig.add_subplot(1,20,20)
-    # ax_cbar.yaxis.set_label_position("right")
     cbar = plt.colorbar(im, cax=ax_cbar)
     ax_cbar.yaxis.tick_right()
     ax_cbar.set_ylabel('Local Density of states (States/eV/$\AA$)')
@@ -106,6 +117,7 @@ def draw_ldos(ldos, extent, vmin=0., vmax=1., ticks=None):
     if ticks is not None:
         cbar.set_ticks(ticks)
         cbar.set_ticklabels(ticks)
+
     plt.show()
 
 if __name__ == '__main__':
@@ -133,26 +145,41 @@ if __name__ == '__main__':
     parser.add_argument("-x","--xrange", nargs=2,
         help="x range with respect to Fermi level. default is 0 40",
         default = [0, 40])
+    parser.add_argument("-v","--vrange", nargs=2,
+        help="x range with respect to Fermi level. default is 0 1.0",
+        default = [0.1, 1.0])
     parser.add_argument("-s","--sigma",
         help="Gaussian with for energy smearing. default is 0.1 eV",
         default = 0.2)
+    parser.add_argument("-a","--axis",
+        help="summation axis (0, 1, 2). default is 0",
+        default = 0)
     parser.add_argument("--force-calc", dest='f_calc',
         help="force to recalculation. default is False",
         default = False, action='store_true')
-    
+    parser.add_argument("--log-scale", dest='log_scale',
+        help="scale log. default is True",
+        default = False, action='store_true')
+
     args = parser.parse_args()
     path = args.path
     e_min, e_max = [float(energy) for energy in args.energy]
-    e_grid = int(args.egrid)
     x_min, x_max = [float(x) for x in args.xrange]
+    vmin, vmax = [float(v) for v in args.vrange]
+    e_grid = int(args.egrid)
     sigma = float(args.sigma)
     f_calc = args.f_calc
     save_name = args.output
+    axis = int(args.axis) % 3
+    log_scale = args.log_scale
 
-    extent = (x_min, x_max, e_min, e_max)
+    extent_calc = (x_min, x_max, e_min - sigma, e_max + sigma)
+    extent_show = (x_min, x_max, e_min, e_max)
 
+    print f_calc
     if f_calc:
-        ldos_interpol = get_ldos(path, extent, sigma=sigma, e_grid=e_grid)
+        ldos_interpol = get_ldos(path, extent_calc, sigma=sigma,
+                                 e_grid=e_grid, axis=axis)
         np.save(path + '/' + save_name, ldos_interpol)
     else:
         try:
@@ -160,8 +187,9 @@ if __name__ == '__main__':
             ldos_interpol = np.load(path + '/' + save_name)
         except IOError:
             print 'no save file. calculation starts'
-            ldos_interpol = get_ldos(path, extent, sigma=sigma, e_grid=e_grid)
-            np.save(path + '/' + save_name, ldos_interpol)
+            ldos_interpol = get_ldos(path, extent_calc, sigma=sigma,
+                                     e_grid=e_grid, axis=axis)
+        np.save(path + '/' + save_name, ldos_interpol)
 
-    draw_ldos(ldos_interpol, (x_min, x_max, e_min + 0.5 , e_max - 0.5),
-              0.0, 0.8, np.arange(10) * 10)
+    draw_ldos(ldos_interpol, extent_calc, extent_show,
+              vmin, vmax, np.arange(10) * 10,  log_scale=log_scale)
